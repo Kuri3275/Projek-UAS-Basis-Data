df52b314282c833b8c002374c37a6ad04859f11f
Kuri3275 <kuri3275@gmail.com>
2026-01-20 13:46:10 +0700

    Create 02_create_table_pelanggan.sql
    Create table pelanggan

---
+++ SQL/02_create_table_pelanggan.sql
@@ -0,0 +1,9 @@
+CREATE TABLE transaksi_awal (
+    no_order VARCHAR(10),
+    tanggal DATE,
+    nama_pelanggan VARCHAR(100),
+    nama_barang VARCHAR(100),
+    harga DECIMAL(10,2),
+    jumlah INT,
+    total DECIMAL(12,2)
+);
